79dd2b7dc4e53926680975a779b637ab3afde046
Kate Sanders <katesanders@HLTCOE-helps-MacBook-Pro.local>
2022-12-04 23:49:01 -0500

    Added text file

---
+++ text.js
@@ -0,0 +1,24 @@
+var text = [{
+    "title": "Graph A",
+    "description": "Description for graph A goes here."
+  }, {
+    "title": "Graph B",
+    "description": "Description for graph B goes here."
+  },
+  {
+    "title": "Graph C",
+    "description": "Description for graph C goes here."
+  },
+  {
+    "title": "Graph D",
+    "description": "Description for graph D goes here."
+  },
+  {
+    "title": "Graph E",
+    "description": "Description for graph E goes here."
+  },
+  {
+    "title": "Graph F",
+    "description": "Description for graph F goes here."
+  },
+  ]
